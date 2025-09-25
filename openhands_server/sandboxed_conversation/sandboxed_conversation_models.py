4cb067a2fd909aab1dc64ec504b3aec819c0047e
Tim O'Farrell <tofarr@gmail.com>
2025-09-24 22:12:25 -0600

    Doc update

--- openhands_server/sandboxed_conversation/sandboxed_conversation_models.py
+++ openhands_server/sandboxed_conversation/sandboxed_conversation_models.py
@@ -16,10 +16,11 @@ class SandboxedConversationPage(BaseModel):
 
 
 class StartSandboxedConversationRequest(BaseModel):
-    """ Although a user can go directly to the sandbox and start conversations, these will lack
+    """Although a user can go directly to the sandbox and start conversations, these will lack
     any of the stored settings for a user, and perhaps some of the initial settings related to git.
-    Starting a conversation in the managing server allows default parameters / secrets to be loaded from 
-    settings. """
+    Starting a conversation in the managing server allows default parameters / secrets to be loaded from
+    settings."""
+
     sandbox_id: str | None = Field(default=None)
-    request: StartConversationRequest #TODO: Since this may be taken from the default, a lot of these settings need to be made optional.
+    request: StartConversationRequest  # TODO: Since this may be taken from the default, a lot of these settings need to be made optional.
     processors: list[EventCallbackProcessor]
