4d6987144ca85e9172962c223bf4543c939433bd
Tim O'Farrell <tofarr@gmail.com>
2025-09-24 22:11:26 -0600

    Doc update

--- openhands_server/sandboxed_conversation/sandboxed_conversation_models.py
+++ openhands_server/sandboxed_conversation/sandboxed_conversation_models.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel, Field
 
 from openhands.agent_server.models import ConversationInfo, StartConversationRequest
+from openhands_server.event_callback.event_callback_models import EventCallbackProcessor
 from openhands_server.sandbox.sandbox_models import SandboxInfo
 
 
@@ -15,6 +16,10 @@ class SandboxedConversationPage(BaseModel):
 
 
 class StartSandboxedConversationRequest(BaseModel):
+    """ Although a user can go directly to the sandbox and start conversations, these will lack
+    any of the stored settings for a user, and perhaps some of the initial settings related to git.
+    Starting a conversation in the managing server allows default parameters / secrets to be loaded from 
+    settings. """
     sandbox_id: str | None = Field(default=None)
-    request: StartConversationRequest
-    callbacks: list[ConversationCallback]
+    request: StartConversationRequest #TODO: Since this may be taken from the default, a lot of these settings need to be made optional.
+    processors: list[EventCallbackProcessor]
