e781ceced0de841d6be2ef4214d7a6419756884d
Tim O'Farrell <tofarr@gmail.com>
2025-09-25 07:24:52 -0600

    Added auth endpoints

---
+++ openhands_server/auth/auth_router.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter
+from fastapi.responses import RedirectResponse
+
+
+router = APIRouter(prefix="/", tags=["Auth"])
+
+
+@router.get("/login")
+def login() -> RedirectResponse:
+    """Redirect into the login flow - not intended for use in a fetch request."""
+    return RedirectResponse("/oauth/callback")
+
+
+@router.get("/oauth/callback")
+def oauth_callback() -> RedirectResponse:
+    return RedirectResponse("/")
+
+
+@router.get("/logout")
+def logout():
+    pass
+
+
+@router.get("/me")
+def me():
+    pass
