9dd79b6be221aa4076dc0fb28f0ab6dec2c392ab
Tim O'Farrell <tofarr@gmail.com>
2025-09-27 09:14:53 -0600

    Adding sandbox check

--- openhands_server/event_callback/event_webhook_router.py
+++ openhands_server/event_callback/event_webhook_router.py
@@ -1,16 +1,19 @@
 """Event Callback router for OpenHands Server."""
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import APIKeyHeader
 
 from openhands.agent_server.models import ConversationInfo
 from openhands.sdk import EventBase
+from openhands_server.dependency import get_dependency_resolver
+from openhands_server.sandbox.sandbox_context import SandboxContext
 
 
 router = APIRouter(prefix="/event-webhooks", tags=["Event Callbacks"])
+sandbox_context_dependency = Depends(get_dependency_resolver().sandbox.get_resolver())
 
-
-# TODO: We need to put into the runtime the correct url for callbacks
+# TODO: I think we need a sandbox service (outside the context of the user)
+#       where we can just load what we need
 
 
 @router.post("/{sandbox_id}/conversations")
@@ -20,10 +23,15 @@ async def on_conversation_update(
     session_api_key: str = Depends(
         APIKeyHeader(name="X-Session-API-Key", auto_error=False)
     ),
+    sandbox_context: SandboxContext = sandbox_context_dependency,
 ):
     """Webhook callback for when a conversation starts, pauses, resumes, or deletes"""
-    # TODO: Load the sandbox and make sure that the session api key matches
-    #       the one provided (Maybe use a dependency?)
+
+    # Check that the session api key is valid for the sandbox
+    sandbox_info = await sandbox_context.get_sandbox(sandbox_id)
+    if sandbox_info.session_api_key != session_api_key:
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED)
+
     # TODO: Make sure that we have an entry saved for the conversation info.
     #       (We will still go back to the sandbox to determine its status,
     #       but we want the metrics.)
@@ -36,8 +44,15 @@ async def on_event(
     session_api_key: str = Depends(
         APIKeyHeader(name="X-Session-API-Key", auto_error=False)
     ),
+    sandbox_context: SandboxContext = sandbox_context_dependency,
 ):
     """Webhook callback for when event stream events occur"""
+
+    # Check that the session api key is valid for the sandbox
+    sandbox_info = await sandbox_context.get_sandbox(sandbox_id)
+    if sandbox_info.session_api_key != session_api_key:
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED)
+
     # TODO: Load the sandbox and make sure that the session api key matches
     #       the one provided (Maybe use a dependency?)
     # TODO: Store each event
